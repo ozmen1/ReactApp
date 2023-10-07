1e51799d218ee25047be493c007424a4874a65a7
ozmen1 <ozmen41@gmail.com>
2023-10-07 20:19:51 +0300

    data transfer between components

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -6,12 +6,7 @@ export default class CategoryList extends Component {
     categories: [
       { categoryId: 1, categoryName: "Beverages" },
       { categoryId: 2, categoryName: "Condiments" },
-    ],
-    currentCategory: "",
-  };
-
-  changeCategory = (category) => {
-    this.setState({ currentCategory: category.categoryName });
+    ]
   };
 
   render() {
@@ -21,14 +16,14 @@ export default class CategoryList extends Component {
         <ListGroup>
           {this.state.categories.map((category) => (
             <ListGroupItem
-              onClick={() => this.changeCategory(category)}
+              onClick={() => this.props.changeCategory(category)}
               key={category.categoryId}
             >
               {category.categoryName}
             </ListGroupItem>
           ))}
         </ListGroup>
-        <h4>{this.state.currentCategory}</h4>
+        <h4>{this.props.currentCategory}</h4>
       </div>
     );
   }
