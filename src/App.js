45348ab69f149d7857629a45e1336f27cd457835
ozmen1 <ozmen41@gmail.com>
2023-10-07 16:59:39 +0300

    initialized project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "User App";
+  const isAuthorized = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h5>{title}</h5>
+      <label htmlFor="name">Name</label>
+      
+      {
+        isAuthorized ? <p>Authorized</p> : <p>Not Authorized</p>
+      }
+
+      <p className="lead">This is a sample text</p>
     </div>
   );
 }
